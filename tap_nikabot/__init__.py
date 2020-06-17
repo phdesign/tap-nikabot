b8b12dbae2f04aa17e333b3cb70a46ddbca70404
Paul Heasley <paul.heasley@pageuppeople.com>
2020-06-17 21:58:54 +1000

    refactor: Starting functional branch

--- tap_nikabot/__init__.py
+++ tap_nikabot/__init__.py
@@ -1,25 +1,94 @@
 #!/usr/bin/env python3
-from typing import Dict
+from typing import Dict, Any, List, Iterator
+import requests
 import singer
-from singer import utils
-from singer.catalog import Catalog
-from .streams import all_streams
-from .client import Client
+from singer import utils, Schema, metadata
+from singer.catalog import Catalog, CatalogEntry
 
 LOGGER = singer.get_logger()
 DEFAULT_CONFIG = {"page_size": 1000}
 REQUIRED_CONFIG_KEYS = ["access_token"]
+BASE_URL = "https://api.nikabot.com"
+MAX_API_PAGES = 10000
+
+
+def fetch_swagger_definition() -> Any:
+    response = requests.get(f"{BASE_URL}/v2/api-docs?group=public")
+    response.raise_for_status()
+    swagger = response.json()
+    return swagger
+
+
+def get_catalog_entry(schema: Schema, stream_id: str, key_properties: List[str], replication_key: str = None) -> CatalogEntry:
+    stream_metadata = metadata.get_standard_metadata(
+        schema.to_dict(),
+        stream_id,
+        key_properties,
+        valid_replication_keys=[replication_key] if replication_key else None,
+    )
+    # Default to selected
+    stream_metadata = metadata.to_list(metadata.write(metadata.to_map(stream_metadata), (), "selected", True))
+    catalog_entry = CatalogEntry(
+        tap_stream_id=stream_id,
+        stream=stream_id,
+        schema=schema,
+        key_properties=key_properties,
+        metadata=stream_metadata,
+        replication_key=replication_key,
+    )
+    return catalog_entry
+
+
+def get_user_catalog_entry(swagger: Any) -> CatalogEntry:
+    schema = Schema.from_dict(swagger["definitions"]["UserDTO"])
+    return get_catalog_entry(schema, "users", ["id"], "updated_at")
+
+
+def get_role_catalog_entry(swagger: Any) -> CatalogEntry:
+    schema = Schema.from_dict(swagger["definitions"]["RoleDTO"])
+    return get_catalog_entry(schema, "roles", ["id"])
 
 
 def discover() -> Catalog:
-    swagger = Client.fetch_swagger_definition()
-    schemas = [stream().get_schema(swagger) for stream in all_streams]
+    swagger = fetch_swagger_definition()
+    schemas = [get_user_catalog_entry(swagger), get_role_catalog_entry(swagger)]
     return Catalog(schemas)
 
 
-def sync(config: Dict[str, str], state: Dict[str, str], catalog: Catalog) -> None:
+def fetch(session: requests.Session, page_size: str, url: str, page_number: int) -> Any:
+    params = {"limit": page_size, "page": page_number}
+    response = session.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def fetch_all(session: requests.Session, page_size: str, url: str) -> Iterator[List[Dict[str, Any]]]:
+    for page_number in range(MAX_API_PAGES):
+        result = fetch(session, page_size, url, page_number)
+        if len(result["result"]) == 0:
+            break
+        yield result["result"]
+
+
+def get_user_records(session: requests.Session, page_size: str) -> Iterator[List[Dict[str, Any]]]:
+    for page in fetch_all(session, page_size, f"{BASE_URL}/api/v1/users"):
+        yield page
+
+
+def get_role_records(session: requests.Session, page_size: str) -> Iterator[List[Dict[str, Any]]]:
+    for page in fetch_all(session, page_size, f"{BASE_URL}/api/v1/roles"):
+        yield page
+
+
+def sync(config: Dict[str, Any], state: Dict[str, Any], catalog: Catalog) -> None:
     """ Sync data from tap source """
-    client = Client(config["access_token"], config["page_size"])
+    session = requests.Session()
+    session.headers.update({"Authorization": f"Bearer {config['access_token']}"})
+    streams = {
+        "users": get_user_records,
+        "roles": get_role_records,
+    }
+
     # Loop over selected streams in catalog
     for selected_stream in catalog.get_selected_streams(state):
         LOGGER.info("Syncing stream: %s", selected_stream.tap_stream_id)
@@ -34,8 +103,8 @@ def sync(config: Dict[str, str], state: Dict[str, str], catalog: Catalog) -> Non
         )
 
         max_bookmark = ""
-        stream = next(s for s in all_streams if s.stream_id == selected_stream.tap_stream_id)
-        for rows in stream().get_records(client):
+        stream = streams[selected_stream.tap_stream_id]
+        for rows in stream(session, config["page_size"]):
             # write one or more rows to the stream:
             singer.write_records(selected_stream.tap_stream_id, rows)
             if bookmark_column:
--- tap_nikabot/streams/__init__.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Type
-
-from .stream import Stream
-from .users import Users
-from .roles import Roles
-from .groups import Groups
-
-all_streams: List[Type[Stream]] = [Users, Roles, Groups]
